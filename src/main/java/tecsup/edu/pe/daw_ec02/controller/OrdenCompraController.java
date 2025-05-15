04c819051423b86bc4ef42d61a23965f77972062
abuenoh-tecsup <alvaro.bueno@tecsup.edu.pe>
2025-05-14 21:05:56 -0500

    Controller OrdenCompraController

---
+++ src/main/java/tecsup/edu/pe/daw_ec02/controller/OrdenCompraController.java
@@ -0,0 +1,47 @@
+package tecsup.edu.pe.daw_ec02.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import tecsup.edu.pe.daw_ec02.model.OrdenCompra;
+import tecsup.edu.pe.daw_ec02.service.OrdenCompraService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ordenes")
+public class OrdenCompraController {
+
+    private final OrdenCompraService ordenCompraService;
+
+    @Autowired
+    public OrdenCompraController(OrdenCompraService ordenCompraService) {
+        this.ordenCompraService = ordenCompraService;
+    }
+
+    @GetMapping
+    public List<OrdenCompra> listar() {
+        return ordenCompraService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public OrdenCompra buscar(@PathVariable Long id) {
+        return ordenCompraService.obtener(id);
+    }
+
+    @PostMapping
+    public void crear(@RequestBody OrdenCompra ordenCompra) {
+        ordenCompraService.crear(ordenCompra);
+    }
+
+    @PutMapping("/{id}")
+    public void actualizar(@PathVariable Long id, @RequestBody OrdenCompra ordenCompra) {
+        ordenCompra.setNroOrdenC(id);
+        ordenCompraService.actualizar(ordenCompra);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        ordenCompraService.eliminar(id);
+    }
+
+}
