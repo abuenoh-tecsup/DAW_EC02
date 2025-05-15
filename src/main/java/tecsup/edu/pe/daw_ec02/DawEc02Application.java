8072e30dfc1d04e961485bb59502f6cd3d897c5a
abuenoh-tecsup <alvaro.bueno@tecsup.edu.pe>
2025-05-14 20:19:03 -0500

    Initial project

---
+++ src/main/java/tecsup/edu/pe/daw_ec02/DawEc02Application.java
@@ -0,0 +1,13 @@
+package tecsup.edu.pe.daw_ec02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawEc02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawEc02Application.class, args);
+    }
+
+}
---
+++ src/test/java/tecsup/edu/pe/daw_ec02/DawEc02ApplicationTests.java
@@ -0,0 +1,13 @@
+package tecsup.edu.pe.daw_ec02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawEc02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
